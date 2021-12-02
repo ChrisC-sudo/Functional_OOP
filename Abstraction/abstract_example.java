04aec2e2899ef2d39e217736a670c65847809d1c
Chris C <50221322+ChrisC-sudo@users.noreply.github.com>
2021-12-02 15:46:24 -0500

    Add files via upload

---
+++ Abstraction/abstract_example.java
@@ -0,0 +1,13 @@
+package Abstraction;
+
+abstract class abstract_example 
+{
+	abstract void chris(); //abstract method
+	
+	public void chris(String c) //concrete method
+	{
+		System.out.println("This is a concrete method of abstract_example class"); //body of concrete method
+	}
+
+}
+
---
+++ Abstraction/abstract_main.java
@@ -0,0 +1,17 @@
+package Abstraction;
+
+public class abstract_main extends abstract_example{
+	public static void main(String[] args) 
+	{
+		abstract_example object =  new abstract_main(); //Accessing data members of abstract_example by using the instance of abstract_main
+		//System.out.println("hello " + object.c);
+		object.chris("c"); //calling concrete method
+		object.chris(); //calling abstract method
+	}
+	@Override
+	void chris() //Body of an abstract method chris() in the subclass i.e abstract_main
+	{
+		System.out.println("Body of an abstract method");
+	}
+
+}
