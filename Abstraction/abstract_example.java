1cf8e0d92c3c53899ec1b38223b736be2239fe0f
Chris C <50221322+ChrisC-sudo@users.noreply.github.com>
2021-12-02 16:41:59 -0500

    Update abstract_example.java

--- Abstraction/abstract_example.java
+++ Abstraction/abstract_example.java
@@ -1,9 +1,3 @@
-/* Abstraction is basically hiding the execution of the internal/sensitive details 
- * from the user and showing the important and required functionality to the users.
- * 
- * For Example: Like driving a car, the owner of the car doesnt need to know the internal implementation of how the car works,
- * but he knows how to drive a car and also familiar with the various functions of the car*/
-
 package Abstraction;
 
 abstract class abstract_example 
